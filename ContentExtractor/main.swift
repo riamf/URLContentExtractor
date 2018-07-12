82a49bcf5a8e93f4d56d073d12d3eaf837641c33
Pawel Kowalczuk <pwl.kowalczuk@gmail.com>
2018-07-12 17:39:01 +0200

    Initial Commit

---
+++ ContentExtractor/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ContentExtractor
+//
+//  Created by Pawel Kowalczuk on 12/07/2018.
+//  Copyright © 2018 Pawel Kowalczuk. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
